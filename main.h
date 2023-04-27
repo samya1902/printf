028a220cdd1922d40882c9cd5991ed5f355fff88
Hassnae BADDOU <samya1902.com>
2023-04-27 07:23:19 -0700

    task 0

--- main.h
+++ main.h
@@ -5,6 +5,33 @@
 #include <stdio.h>
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+#define S_LONG 2
+#define S_SHORT 1
+
+/**
+ * struct fmt - Struct op
+ * @fmt: The format.
+ * @fn: The function associated.
+ */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+/**
+ * typedef struct fmt fmt_t - Struct op
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+typedef struct fmt fmt_t;
+int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
 int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
@@ -14,4 +41,4 @@ int print_unsigned(va_list types, char buffer[], int flags, int width, int preci
 int print_octal(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[], int flags, int width, int precision, int size);
-#endif
+#endiif
